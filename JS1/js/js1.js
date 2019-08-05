21a12d3adfc4b8696b647181d94e9596ecf26c66
egidum <sixpilot80@yahoo.com>
2019-08-06 00:50:45 +0300

    1.0.20190806 Pirma versija (neišsiveda į konsolę)

---
+++ JS1/js/js1.js
@@ -0,0 +1,9 @@
+let PirmasSkaicius = 10;
+let AntrasSkaicius = 20;
+
+function dviejuSkaiciuSuma(PirmasSkaicius, AntrasSkaicius){
+    let skaiciuSuma = PirmasSkaicius + AntrasSkaicius;
+    console.log(skaiciuSuma);
+    document.write(skaiciuSuma);
+    alert(skaiciuSuma);
+}
