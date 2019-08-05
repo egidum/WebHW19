54a0ddb8f3be037bc67da51c8ba6809c69dcbfb6
egidum <sixpilot80@yahoo.com>
2019-08-05 19:12:46 +0300

    1.0.20190805 Pirma versija (daryta klaseje)

---
+++ JS5/js/js5.js
@@ -0,0 +1,13 @@
+function arPilnametis(amzius){
+    if(amzius >= 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+function arPilnametis2(amzius){
+    return amzius >= 18;
+}
