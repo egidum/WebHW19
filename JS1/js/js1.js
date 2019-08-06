ede853372dceaa00651231e6b88bd63762afe4c7
egidum <sixpilot80@yahoo.com>
2019-08-06 08:54:48 +0300

    1.1.20190806 Supaprastintas (neįšveda į konsolę)

--- JS1/js/js1.js
+++ JS1/js/js1.js
@@ -1,9 +1,9 @@
 let PirmasSkaicius = 10;
 let AntrasSkaicius = 20;
 
-function dviejuSkaiciuSuma(PirmasSkaicius, AntrasSkaicius){
+{
     let skaiciuSuma = PirmasSkaicius + AntrasSkaicius;
-    console.log(skaiciuSuma);
-    document.write(skaiciuSuma);
-    alert(skaiciuSuma);
+        console.log(skaiciuSuma);
+        document.write(skaiciuSuma);
+        alert(skaiciuSuma);
 }
