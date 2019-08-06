51460421806d4363b8c0303e060ad1581d18ece4
egidum <sixpilot80@yahoo.com>
2019-08-06 08:57:16 +0300

    1.2.20190806 Supaprastintas (neišveda į konsolę)

--- JS1/js/js1.js
+++ JS1/js/js1.js
@@ -1,9 +1,9 @@
 let PirmasSkaicius = 10;
 let AntrasSkaicius = 20;
 
-{
-    let skaiciuSuma = PirmasSkaicius + AntrasSkaicius;
-        console.log(skaiciuSuma);
-        document.write(skaiciuSuma);
-        alert(skaiciuSuma);
-}
+
+let skaiciuSuma = PirmasSkaicius + AntrasSkaicius;
+    console.log(skaiciuSuma);
+    document.write(skaiciuSuma);
+    alert(skaiciuSuma);
+
