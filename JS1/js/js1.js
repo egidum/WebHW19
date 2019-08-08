5486bbb694e9ccb8f6724b3d7621c57cf6d1e15f
egidum <sixpilot80@yahoo.com>
2019-08-08 13:39:35 +0300

    2.0.20190808 Pataisyta (atskirame faile fn() ir sutvarkytas html)

--- JS1/js/js1.js
+++ JS1/js/js1.js
@@ -1,9 +1,4 @@
-let pirmasSkaicius = 10;
-let antrasSkaicius = 20;
-
-
-let skaiciuSuma = pirmasSkaicius + antrasSkaicius;
-    console.log(skaiciuSuma);
-    document.write(skaiciuSuma);
-    alert(skaiciuSuma);
-
+function sudetis(x, y){
+   let skaiciuSuma = x + y;
+   document.write(skaiciuSuma + "<br>");
+}
